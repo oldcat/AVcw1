3e8b5117f55fe48299d378bebfe35441eb083d16
Andrew Burnie <a.burnie@sms.ed.ac.uk>
2012-03-01 17:10:06 +0000

    ...

--- complexmoment.m
+++ complexmoment.m
@@ -1,13 +1,13 @@
 % gets a given complex central moment value
 function muv = complexmoment(Image,u,v)
 
-     [r,c] = find(Image==1);            % get (r,c) of region's pixels
-     rbar = mean(r);
-     cbar = mean(c);
-     n = length(r);
-     momlist = zeros(n,1);
-     for i = 1 : n
-       c1 = complex(r(i) - rbar,c(i) - cbar);
+     [r,c] = find(Image>0);     % get (r,c) of region's pixels
+     rbar = mean(r);            % get centre row of where there's stuff
+     cbar = mean(c);            % get centre col of where there's stuff
+     n = length(r);             % get number of pixels over 0
+     momlist = zeros(n,1);      % initialise list with space for value relating to each non zero pixel
+     for i = 1 : n                              
+       c1 = complex(r(i) - rbar,c(i) - cbar);   
        c2 = complex(r(i) - rbar,cbar - c(i));
        momlist(i) = c1^u * c2^v;
      end
