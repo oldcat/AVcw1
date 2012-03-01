54ee49c6524e7be703bd34b055b42cd19ec405d3
Andrew Burnie <a.burnie@sms.ed.ac.uk>
2012-03-01 16:26:17 +0000

    MHI Works we think

--- motionHistoryImage.m
+++ motionHistoryImage.m
@@ -1,18 +1,24 @@
 function [motionHistory] = motionHistoryImage(binaries)
 
 	num = size(binaries);
-	avg = double(binaries(:,:,1));
+	avg = double(binaries(:,:,1))
 
     total = num(3);
 
+    fail = 0;
+    failEnd = 0;
+
 	for i = 2:num(3)
-	    if ((i <= 3) & ~sum(sum(avg & binaries(:,:,i))))
+	    if ((i <= 4) & ~sum(sum(avg & binaries(:,:,i))))
     	    avg = double(binaries(:,:,i));
-    	    total = total-i+1;
-        elseif ((i >= num-3) & ~sum(sum(avg & binaries(:,:,i))))
-            total = total - (num-i+1);
+    	    total = total-(i-1-fail);
+    	    fail = i-1;
+        elseif ((i >= num-2) & sum(sum(avg & binaries(:,:,i))))
+       		avg = avg + double(binaries(:,:,i));
+        elseif ((i >= num-2) & ~sum(sum(avg & binaries(:,:,i))))
+            total = total - (num(3)-i+1);
             break;
-        else
+        elseif (i < num-2);
        		avg = avg + double(binaries(:,:,i));
         end
 	end
--- showAllBoundingBoxesScript.m
+++ showAllBoundingBoxesScript.m
@@ -6,6 +6,6 @@ seqs = listSeqs(path);
 
 dim = size(seqs);
 
-for i = 10:10 %dim(1)
+for i = 1:dim(1)
     boundingBoxTest(seqs(i,:), i, 1, path);
 end
